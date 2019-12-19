6c016fdd8bcdf64457f4bb22d95602da26cb4aa4
Alexandra Caulea <alexandra.caulea@gmail.com>
2019-12-19 18:39:11 +0200

    Updates

---
+++ precache-manifest.ba5417fad3211c33727263fc15739644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297b22ef7027eeffba03abc75f8b023a",
+    "url": "/random-emoji-generator/index.html"
+  },
+  {
+    "revision": "f4d01d03cfa7d280bdb0",
+    "url": "/random-emoji-generator/static/css/main.fc4e30d8.chunk.css"
+  },
+  {
+    "revision": "9db5b815830b7dc47ad8",
+    "url": "/random-emoji-generator/static/js/2.6c5bf649.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-emoji-generator/static/js/2.6c5bf649.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4d01d03cfa7d280bdb0",
+    "url": "/random-emoji-generator/static/js/main.230950cd.chunk.js"
+  },
+  {
+    "revision": "4cf409265cdac9fcb05b",
+    "url": "/random-emoji-generator/static/js/runtime-main.61829de5.js"
+  }
+]);
